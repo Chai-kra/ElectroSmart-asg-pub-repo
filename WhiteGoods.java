f19dc8aa602d0a8c8cc4d21f738766c52dc80b82
ZIQILEONG <ziqileong005@gmail.com>
2026-08-16 01:28:31 +0800

    update WhiteGoods.java

--- WhiteGoods.java
+++ WhiteGoods.java
@@ -1,3 +1,20 @@
-public class WhiteGoods {
-    
+public class WhiteGoods extends Appliance {
+    private String energyRating;
+    private String dimension;
+    private static final double DELIVERY_SURCHARGE = 50.0;
+
+    public WhiteGoods(String applianceID, String modelName, String brand, double basePrice, int stockQuantity,
+                       String energyRating, String dimension) {
+        super(applianceID, modelName, brand, basePrice, stockQuantity);
+        this.energyRating = energyRating;
+        this.dimension = dimension;
+    }
+
+    public String getEnergyRating() { return energyRating; }
+    public String getDimension() { return dimension; }
+
+    @Override
+    public double calculateFinalPrice() {
+        return getBasePrice() + DELIVERY_SURCHARGE;
+    }
 }
