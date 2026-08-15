ee6cf6d97d70bcd267651c936c33e2cf1cebc748
ZIQILEONG <ziqileong005@gmail.com>
2026-08-16 01:29:06 +0800

    update DigitalGadgets.java

--- DigitalGadgets.java
+++ DigitalGadgets.java
@@ -1,3 +1,20 @@
-public class DigitalGadgets {
-    
+public class DigitalGadgets extends Appliance {
+    private String operatingSystem;
+    private double powerConsumption;
+    private static final double RECYCLING_LEVY_RATE = 0.02; // 2% of base price
+
+    public DigitalGadgets(String applianceID, String modelName, String brand, double basePrice, int stockQuantity,
+                           String operatingSystem, double powerConsumption) {
+        super(applianceID, modelName, brand, basePrice, stockQuantity);
+        this.operatingSystem = operatingSystem;
+        this.powerConsumption = powerConsumption;
+    }
+
+    public String getOperatingSystem() { return operatingSystem; }
+    public double getPowerConsumption() { return powerConsumption; }
+
+    @Override
+    public double calculateFinalPrice() {
+        return getBasePrice() + (getBasePrice() * RECYCLING_LEVY_RATE);
+    }
 }
